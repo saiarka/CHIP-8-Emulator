a3e911f9d6dcbf3f0f43decd36940cec0db06741
Raja Madupu <raja.madupu@gmail.com>
2025-12-18 19:20:12 -0500

    CMakeLists files, src, build-set up files added

---
+++ CHIP8-Emulator/src/main.cpp
@@ -0,0 +1,13 @@
+﻿// CHIP8-Emulator.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+
+}
